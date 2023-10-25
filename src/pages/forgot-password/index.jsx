6e8cb49d1ac78699ae3caf8acd7b88a42c5b8375
Corey Taylor <taylorcoreyc@gmail.com>
2023-10-25 15:12:24 -0700

    Editted

--- src/pages/forgot-password/index.jsx
+++ src/pages/forgot-password/index.jsx
@@ -26,10 +26,7 @@ export default function ForgotPassword() {
             Back
          </Link>
          <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <div className="flex items-center justify-center text-5xl font-bold text-[#D71E28] cursor-default pb-2">
-               {" "}
-               ACU
-            </div>
+            <div className="flex items-center justify-center text-5xl font-bold text-[#D71E28] cursor-default pb-2">ACU</div>
             <div className="w-full p-6 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md dark:bg-gray-800 dark:border-gray-700 sm:p-8">
                <h2 className="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                   Forgot Password
@@ -39,14 +36,7 @@ export default function ForgotPassword() {
                      <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                         Enter your email
                      </label>
-                     <input
-                        type="email"
-                        name="email"
-                        id="email"
-                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="name@company.com"
-                        required=""
-                     />
+                     <input type="email" name="email" id="email" placeholder="name@company.com" required="" />
                   </div>
 
                   <button
