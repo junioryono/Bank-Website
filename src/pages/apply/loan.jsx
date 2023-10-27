8d89f28609306ec26476a4440f4f54d5e28795ba
Corey Taylor <taylorcoreyc@gmail.com>
2023-10-27 11:56:11 -0700

    Padding

--- src/pages/apply/loan.jsx
+++ src/pages/apply/loan.jsx
@@ -178,7 +178,7 @@ export default function Loan() {
                </div>
             </div>
 
-            <div className="border-b border-gray-900/10 pb-12">
+            <div className="border-b border-gray-900/10 pb-12 pt-10">
                <h2 className="text-base font-semibold leading-7 text-gray-900">Notifications</h2>
                <p className="mt-1 text-sm leading-6 text-gray-600">
                   We'll always let you know about important changes, but you pick what else you want to hear about.
