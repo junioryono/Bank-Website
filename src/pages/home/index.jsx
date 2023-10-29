a70268c36fbe2f9a115aaf787749169c8b6b5315
Abdalla <abx99@hotmail.com>
2023-10-29 00:09:32 -0700

    Addef navbar for the home page.

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,3 +1,35 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
-   return <div className="text-3xl font-bold underline text-primary">Home</div>;
+   const navBarItems = [
+      "Checking",
+      "Savings & CDs",
+      "Credit Cards",
+      "Home",
+      "Loans",
+      "Personal",
+      "Loans",
+      "Auto",
+      "Loans",
+      "Premier",
+      "Education & Tools",
+   ];
+   return (
+      <>
+         <nav className="flex bg-center gap-20 border-b-2">
+            <Link to={"create/checkings"} className="hover:bg-orange-50 hover:underline rounded-md px-3 py-3">
+               Checking
+            </Link>
+            <Link to={"create/savings"} className="hover:bg-orange-50 hover:underline rounded-md px-3 py-3">
+               Savings
+            </Link>
+            <Link to={"apply/credit-card"} className="hover:bg-orange-50 hover:underline rounded-md px-3 py-3">
+               Credit Cards
+            </Link>
+            <Link to={"apply/loan"} className="hover:bg-orange-50 hover:underline rounded-md px-3 py-3">
+               Loans
+            </Link>
+         </nav>
+      </>
+   );
 }
