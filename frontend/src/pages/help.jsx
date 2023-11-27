4e7b79c8b260a635046acc0dca3e0e7bb633b714
Corey Taylor <taylo278@cougars.csusm.edu>
2023-11-27 00:14:54 -0800

    Edited the help page

--- frontend/src/pages/help.jsx
+++ frontend/src/pages/help.jsx
@@ -1,75 +1,60 @@
+import React, { useState } from "react";
+
+// React component for FontAwesome icon
+const PlusIcon = () => <span className="mr-2">&#43;</span>; // You can replace this with your actual FontAwesome component
+const MinusIcon = () => <span className="mr-2">&#8211;</span>; // You can replace this with your actual FontAwesome component
+
 export default function Help() {
+   const [openSection, setOpenSection] = useState(null);
+
+   const toggleSection = (index) => {
+      setOpenSection((prevOpenSection) => (prevOpenSection === index ? null : index));
+   };
+
    return (
       <section className="bg-white">
          <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
             <h2 className="mb-8 text-4xl font-extrabold text-gray-900">Frequently asked questions</h2>
-            <div class="relative mb-3">
-  <h6 class="mb-0">
-    <button
-      class="border-slate-100 text-slate-700 rounded-t-1 group relative flex w-full cursor-pointer items-center border-b border-solid p-4 text-left font-semibold text-dark-500 transition-all ease-in"
-      data-collapse-target="collapse-1"
-    >
-      <span>What is Material Tailwind?</span>
-      <i class="fa fa-plus absolute right-0 pt-1 text-xs group-open:opacity-0"></i>
-      <i class="fa fa-minus absolute right-0 pt-1 text-xs opacity-0 group-open:opacity-100"></i>
-    </button>
-  </h6>
-  <div
-    data-collapse="collapse-1"
-    class="h-0 overflow-hidden transition-all duration-300 ease-in-out"
-  >
-    <div class="p-4 text-sm leading-normal text-blue-gray-500/80">
-      We're not always in the position that we want to be at. We're constantly
-      growing. We're constantly making mistakes. We're constantly trying to
-      express ourselves and actualize our dreams.
-    </div>
-  </div>
-</div>
-<div class="relative mb-3">
-  <h6 class="mb-0">
-    <button
-      class="border-slate-100 text-slate-700 rounded-t-1 group relative flex w-full cursor-pointer items-center border-b border-solid p-4 text-left font-semibold text-dark-500 transition-all ease-in"
-      data-collapse-target="collapse-2"
-    >
-      <span>How to use Material Tailwind?</span>
-      <i class="fa fa-plus absolute right-0 pt-1 text-xs group-open:opacity-0"></i>
-      <i class="fa fa-minus absolute right-0 pt-1 text-xs opacity-0 group-open:opacity-100"></i>
-    </button>
-  </h6>
-  <div
-    data-collapse="collapse-2"
-    class="h-0 overflow-hidden transition-all duration-300 ease-in-out"
-  >
-    <div class="p-4 text-sm leading-normal text-blue-gray-500/80">
-      We're not always in the position that we want to be at. We're constantly
-      growing. We're constantly making mistakes. We're constantly trying to
-      express ourselves and actualize our dreams.
-    </div>
-  </div>
-</div>
-<div class="relative mb-3">
-  <h6 class="mb-0">
-    <button
-      class="border-slate-100 text-slate-700 rounded-t-1 group relative flex w-full cursor-pointer items-center border-b border-solid p-4 text-left font-semibold text-dark-500 transition-all ease-in"
-      data-collapse-target="collapse-3"
-    >
-      <span>What can I do with Material Tailwind?</span>
-      <i class="fa fa-plus absolute right-0 pt-1 text-xs group-open:opacity-0"></i>
-      <i class="fa fa-minus absolute right-0 pt-1 text-xs opacity-0 group-open:opacity-100"></i>
-    </button>
-  </h6>
-  <div
-    data-collapse="collapse-3"
-    class="h-0 overflow-hidden transition-all duration-300 ease-in-out"
-  >
-    <div class="p-4 text-sm leading-normal text-blue-gray-500/80">
-      We're not always in the position that we want to be at. We're constantly
-      growing. We're constantly making mistakes. We're constantly trying to
-      express ourselves and actualize our dreams.
-    </div>
-  </div>
-</div>
-            </div>
+            {faqData.map((faq, index) => (
+               <div key={index} className="relative mb-3">
+                  <h6 className="mb-0">
+                     <button
+                        onClick={() => toggleSection(index)}
+                        className="border-slate-100 text-slate-700 rounded-t-1 group relative flex w-full cursor-pointer items-center border-b border-solid p-4 text-left font-semibold text-dark-500 transition-all ease-in"
+                     >
+                        <span>{faq.question}</span>
+                        {openSection === index ? <MinusIcon /> : <PlusIcon />}
+                     </button>
+                  </h6>
+                  <div
+                     className={`${
+                        openSection === index ? "block" : "h-0 overflow-hidden"
+                     } transition-all duration-300 ease-in-out`}
+                  >
+                     <div className="p-4 text-sm leading-normal text-blue-gray-500/80">{faq.answer}</div>
+                  </div>
+               </div>
+            ))}
+         </div>
       </section>
    );
 }
+
+// Example FAQ data
+const faqData = [
+   {
+      question: "What is Material Tailwind?",
+      answer:
+         "We're not always in the position that we want to be at. We're constantly growing. We're constantly making mistakes. We're constantly trying to express ourselves and actualize our dreams.",
+   },
+   {
+      question: "How to use Material Tailwind?",
+      answer:
+         "We're not always in the position that we want to be at. We're constantly growing. We're constantly making mistakes. We're constantly trying to express ourselves and actualize our dreams.",
+   },
+   {
+      question: "What can I do with Material Tailwind?",
+      answer:
+         "We're not always in the position that we want to be at. We're constantly growing. We're constantly making mistakes. We're constantly trying to express ourselves and actualize our dreams.",
+   },
+];
