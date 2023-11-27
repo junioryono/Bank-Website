fab4d430b7da85e1129d6985b90a486be4e1e325
arely <arely_nieblas@yahoo.com>
2023-11-27 00:06:26 -0800

    Testing accordion style

--- frontend/src/pages/help.jsx
+++ frontend/src/pages/help.jsx
@@ -3,178 +3,73 @@ export default function Help() {
       <section className="bg-white">
          <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
             <h2 className="mb-8 text-4xl font-extrabold text-gray-900">Frequently asked questions</h2>
-            <div className="grid pt-8 text-left border-t border-gray-200 md:gap-16 md:grid-cols-2">
-               <div>
-                  <div className="mb-10">
-                     <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900">
-                        <svg
-                           className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500"
-                           fill="currentColor"
-                           viewBox="0 0 20 20"
-                           xmlns="http://www.w3.org/2000/svg"
-                        >
-                           <path
-                              fill-rule="evenodd"
-                              d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                              clip-rule="evenodd"
-                           ></path>
-                        </svg>
-                        How do I check my bank statements?
-                     </h3>
-                     <p className="text-gray-500">
-                        You can view your bank statements by clicking on the "Dashboard" tab on the left side of the screen.
-                     </p>
-                  </div>
-                  <div className="mb-10">
-                     <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900">
-                        <svg
-                           className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500"
-                           fill="currentColor"
-                           viewBox="0 0 20 20"
-                           xmlns="http://www.w3.org/2000/svg"
-                        >
-                           <path
-                              fill-rule="evenodd"
-                              d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                              clip-rule="evenodd"
-                           ></path>
-                        </svg>
-                        How do I create a checkings account?
-                     </h3>
-                     <p className="text-gray-500">
-                        You can create a checkings account by clicking on the "Create Account" button on the bottom of the
-                        dashboard page.
-                     </p>
-                  </div>
-                  <div className="mb-10">
-                     <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900">
-                        <svg
-                           className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500"
-                           fill="currentColor"
-                           viewBox="0 0 20 20"
-                           xmlns="http://www.w3.org/2000/svg"
-                        >
-                           <path
-                              fill-rule="evenodd"
-                              d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                              clip-rule="evenodd"
-                           ></path>
-                        </svg>
-                        Am I able to have atomatic banking transfers?
-                     </h3>
-                     <p className="text-gray-500">
-                        Yes, you can set up automatic banking transfers by clicking on the "Transfers" button in the
-                        dashboard page.
-                     </p>
-                  </div>
-                  <div className="mb-10">
-                     <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900">
-                        <svg
-                           className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500"
-                           fill="currentColor"
-                           viewBox="0 0 20 20"
-                           xmlns="http://www.w3.org/2000/svg"
-                        >
-                           <path
-                              fill-rule="evenodd"
-                              d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                              clip-rule="evenodd"
-                           ></path>
-                        </svg>
-                        How do I apply for a loan?
-                     </h3>
-                     <p className="text-gray-500">
-                        You can apply for a loan by clicking on the "Apply for Loan" button on the bottom of the dashboard.
-                     </p>
-                  </div>
-               </div>
-               <div>
-                  <div className="mb-10">
-                     <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900">
-                        <svg
-                           className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500"
-                           fill="currentColor"
-                           viewBox="0 0 20 20"
-                           xmlns="http://www.w3.org/2000/svg"
-                        >
-                           <path
-                              fill-rule="evenodd"
-                              d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                              clip-rule="evenodd"
-                           ></path>
-                        </svg>
-                        How do I apply for a credit card?
-                     </h3>
-                     <p className="text-gray-500">
-                        You can apply for a credit card by clicking on the "Apply for Credit Card" button on the bottom of
-                        the dashboard.
-                     </p>
-                  </div>
-                  <div className="mb-10">
-                     <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900">
-                        <svg
-                           className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500"
-                           fill="currentColor"
-                           viewBox="0 0 20 20"
-                           xmlns="http://www.w3.org/2000/svg"
-                        >
-                           <path
-                              fill-rule="evenodd"
-                              d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                              clip-rule="evenodd"
-                           ></path>
-                        </svg>
-                        Why should I use ACU?
-                     </h3>
-                     <p className="text-gray-500">
-                        ACU is a great way to manage your finances and keep track of your money.
-                     </p>
-                  </div>
-                  <div className="mb-10">
-                     <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900">
-                        <svg
-                           className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500"
-                           fill="currentColor"
-                           viewBox="0 0 20 20"
-                           xmlns="http://www.w3.org/2000/svg"
-                        >
-                           <path
-                              fill-rule="evenodd"
-                              d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                              clip-rule="evenodd"
-                           ></path>
-                        </svg>
-                        What if I want to delete a checkings/savings account?
-                     </h3>
-                     <p className="text-gray-500">
-                        You can delete a checkings/savings account by clicking on the "Delete Account" button on the bottom
-                        of the account information page.
-                     </p>
-                  </div>
-                  <div className="mb-10">
-                     <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900">
-                        <svg
-                           className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500"
-                           fill="currentColor"
-                           viewBox="0 0 20 20"
-                           xmlns="http://www.w3.org/2000/svg"
-                        >
-                           <path
-                              fill-rule="evenodd"
-                              d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                              clip-rule="evenodd"
-                           ></path>
-                        </svg>
-                        How do I create a savings account?
-                     </h3>
-                     <p className="text-gray-500">
-                        You can create a savings account by clicking on the "Create Account" button on the bottom of the
-                        dashboard page.
-                     </p>
-                  </div>
-               </div>
+            <div class="relative mb-3">
+  <h6 class="mb-0">
+    <button
+      class="border-slate-100 text-slate-700 rounded-t-1 group relative flex w-full cursor-pointer items-center border-b border-solid p-4 text-left font-semibold text-dark-500 transition-all ease-in"
+      data-collapse-target="collapse-1"
+    >
+      <span>What is Material Tailwind?</span>
+      <i class="fa fa-plus absolute right-0 pt-1 text-xs group-open:opacity-0"></i>
+      <i class="fa fa-minus absolute right-0 pt-1 text-xs opacity-0 group-open:opacity-100"></i>
+    </button>
+  </h6>
+  <div
+    data-collapse="collapse-1"
+    class="h-0 overflow-hidden transition-all duration-300 ease-in-out"
+  >
+    <div class="p-4 text-sm leading-normal text-blue-gray-500/80">
+      We're not always in the position that we want to be at. We're constantly
+      growing. We're constantly making mistakes. We're constantly trying to
+      express ourselves and actualize our dreams.
+    </div>
+  </div>
+</div>
+<div class="relative mb-3">
+  <h6 class="mb-0">
+    <button
+      class="border-slate-100 text-slate-700 rounded-t-1 group relative flex w-full cursor-pointer items-center border-b border-solid p-4 text-left font-semibold text-dark-500 transition-all ease-in"
+      data-collapse-target="collapse-2"
+    >
+      <span>How to use Material Tailwind?</span>
+      <i class="fa fa-plus absolute right-0 pt-1 text-xs group-open:opacity-0"></i>
+      <i class="fa fa-minus absolute right-0 pt-1 text-xs opacity-0 group-open:opacity-100"></i>
+    </button>
+  </h6>
+  <div
+    data-collapse="collapse-2"
+    class="h-0 overflow-hidden transition-all duration-300 ease-in-out"
+  >
+    <div class="p-4 text-sm leading-normal text-blue-gray-500/80">
+      We're not always in the position that we want to be at. We're constantly
+      growing. We're constantly making mistakes. We're constantly trying to
+      express ourselves and actualize our dreams.
+    </div>
+  </div>
+</div>
+<div class="relative mb-3">
+  <h6 class="mb-0">
+    <button
+      class="border-slate-100 text-slate-700 rounded-t-1 group relative flex w-full cursor-pointer items-center border-b border-solid p-4 text-left font-semibold text-dark-500 transition-all ease-in"
+      data-collapse-target="collapse-3"
+    >
+      <span>What can I do with Material Tailwind?</span>
+      <i class="fa fa-plus absolute right-0 pt-1 text-xs group-open:opacity-0"></i>
+      <i class="fa fa-minus absolute right-0 pt-1 text-xs opacity-0 group-open:opacity-100"></i>
+    </button>
+  </h6>
+  <div
+    data-collapse="collapse-3"
+    class="h-0 overflow-hidden transition-all duration-300 ease-in-out"
+  >
+    <div class="p-4 text-sm leading-normal text-blue-gray-500/80">
+      We're not always in the position that we want to be at. We're constantly
+      growing. We're constantly making mistakes. We're constantly trying to
+      express ourselves and actualize our dreams.
+    </div>
+  </div>
+</div>
             </div>
-         </div>
       </section>
    );
 }
