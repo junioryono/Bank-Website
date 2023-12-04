0a2c03ba22085d1f324360938b32427f8b4c5f91
Abdalla <abx99@hotmail.com>
2023-12-04 14:29:52 -0800

    Added JWT API's for user log in and profile data

--- backend/aztec_credit_union/urls.py
+++ backend/aztec_credit_union/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('users.urls')),
+    path('users/', include('users.urls')),
     path('admin/', admin.site.urls),
 ]
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,5 +1,35 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import AbstractUser
 
-class User(models.Model):
-  firstname = models.CharField(max_length=255)
-  lastname = models.CharField(max_length=255)
+
+class User(AbstractUser):
+    username = models.CharField(max_length=100, default='default')
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def profile(self):
+        profile = Profile.objects.get(user=self)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=1000)
+    address = models.CharField(max_length=255)
+    salary = models.CharField(max_length=255, default='none')
+    image_path = models.CharField(max_length=255, default="default.jpg")
+
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
+post_save.connect(create_user_profile, sender=User)
+post_save.connect(save_user_profile, sender=User)
