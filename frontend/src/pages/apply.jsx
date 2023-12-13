b83fb0572c0cc6a716ad63101a56c4868b6ba3ea
Corey Taylor <taylo278@cougars.csusm.edu>
2023-12-13 13:52:29 -0800

    Changed apply form

--- frontend/src/pages/apply.jsx
+++ frontend/src/pages/apply.jsx
@@ -79,7 +79,7 @@ export default function Apply({ type }) {
                <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                   <div className="sm:col-span-3">
                      <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
-                        First name
+                        Income
                      </label>
                      <div className="mt-2">
                         <input
@@ -95,7 +95,7 @@ export default function Apply({ type }) {
 
                   <div className="sm:col-span-3">
                      <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">
-                        Last name
+                        Credit Limit
                      </label>
                      <div className="mt-2">
                         <input
@@ -109,129 +109,31 @@ export default function Apply({ type }) {
                      </div>
                   </div>
 
-                  <div className="sm:col-span-4">
-                     <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                        Email address
-                     </label>
-                     <div className="mt-2">
-                        <input
-                           id="email"
-                           name="email"
-                           type="email"
-                           autoComplete="email"
-                           className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
-                           onChange={handleInputChange}
-                        />
-                     </div>
-                  </div>
-
                   <div className="sm:col-span-3">
-                     <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">
-                        Country
-                     </label>
-                     <div className="mt-2">
-                        <select
-                           id="country"
-                           name="country"
-                           autoComplete="country-name"
-                           className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset sm:max-w-xs sm:text-sm sm:leading-6"
-                           onChange={handleInputChange}
-                        >
-                           <option>United States</option>
-                           <option>Canada</option>
-                           <option>Mexico</option>
-                        </select>
-                     </div>
-                  </div>
-
-                  <div className="col-span-full">
-                     <label htmlFor="street-address" className="block text-sm font-medium leading-6 text-gray-900">
-                        Street address
+                     <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
+                        Social Security Number (SSN)
                      </label>
                      <div className="mt-2">
                         <input
                            type="text"
-                           name="streetAddress"
-                           id="street-address"
-                           autoComplete="street-address"
+                           name="firstName"
+                           id="first-name"
+                           autoComplete="given-name"
                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
                            onChange={handleInputChange}
                         />
                      </div>
                   </div>
-
-                  <div className="sm:col-span-2 sm:col-start-1">
-                     <label htmlFor="city" className="block text-sm font-medium leading-6 text-gray-900">
-                        City
+                  <div className="sm:col-span-3">
+                     <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
+                        Annual Percentage Rate (APR)
                      </label>
                      <div className="mt-2">
                         <input
                            type="text"
-                           name="city"
-                           id="city"
-                           autoComplete="address-level2"
-                           className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
-                           onChange={handleInputChange}
-                        />
-                     </div>
-                  </div>
-
-                  <div className="sm:col-span-2">
-                     <label htmlFor="region" className="block text-sm font-medium leading-6 text-gray-900">
-                        State / Province
-                     </label>
-                     <div className="mt-2">
-                        <input
-                           type="text"
-                           name="region"
-                           id="region"
-                           autoComplete="address-level1"
-                           className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
-                           onChange={handleInputChange}
-                        />
-                     </div>
-                  </div>
-
-                  <div className="sm:col-span-2">
-                     <label htmlFor="postal-code" className="block text-sm font-medium leading-6 text-gray-900">
-                        ZIP / Postal code
-                     </label>
-                     <div className="mt-2">
-                        <input
-                           type="text"
-                           name="postalCode"
-                           id="postal-code"
-                           autoComplete="postal-code"
-                           className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
-                           onChange={handleInputChange}
-                        />
-                     </div>
-                  </div>
-                  <div className="sm:col-span-2">
-                     <label htmlFor="postal-code" className="block text-sm font-medium leading-6 text-gray-900">
-                        Annual Salary
-                     </label>
-                     <div className="mt-2">
-                        <input
-                           type="text"
-                           name="annualSalary"
-                           id="postal-code"
-                           autoComplete="postal-code"
-                           className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
-                           onChange={handleInputChange}
-                        />
-                     </div>
-                  </div>
-                  <div className="sm:col-span-2">
-                     <label htmlFor="postal-code" className="block text-sm font-medium leading-6 text-gray-900">
-                        SSN
-                     </label>
-                     <div className="mt-2">
-                        <input
-                           type="password"
-                           name="ssn"
-                           id="postal-code"
-                           autoComplete="postal-code"
+                           name="firstName"
+                           id="first-name"
+                           autoComplete="given-name"
                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
                            onChange={handleInputChange}
                         />
@@ -239,95 +141,6 @@ export default function Apply({ type }) {
                   </div>
                </div>
             </div>
-
-            <div className="border-b border-gray-900/10 pb-12 pt-10">
-               <h2 className="text-base font-semibold leading-7 text-gray-900">Notifications</h2>
-               <p className="mt-1 text-sm leading-6 text-gray-600">
-                  We'll always let you know about important changes, but you pick what else you want to hear about.
-               </p>
-
-               <div className="mt-10 space-y-10">
-                  <fieldset>
-                     <legend className="text-sm font-semibold leading-6 text-gray-900">By Email</legend>
-                     <div className="mt-6 space-y-6">
-                        <div className="relative flex gap-x-3">
-                           <div className="flex h-6 items-center">
-                              <input
-                                 id="comments"
-                                 name="comments"
-                                 type="checkbox"
-                                 className="h-4 w-4 rounded border-gray-300"
-                              />
-                           </div>
-                           <div className="text-sm leading-6">
-                              <label htmlFor="comments" className="font-medium text-gray-900">
-                                 Rate Changes
-                              </label>
-                              <p className="text-gray-500">Get notified when rates change for different loans.</p>
-                           </div>
-                        </div>
-                        <div className="relative flex gap-x-3">
-                           <div className="flex h-6 items-center">
-                              <input
-                                 id="candidates"
-                                 name="candidates"
-                                 type="checkbox"
-                                 className="h-4 w-4 rounded border-gray-300"
-                              />
-                           </div>
-                           <div className="text-sm leading-6">
-                              <label htmlFor="candidates" className="font-medium text-gray-900">
-                                 Payment Reminders
-                              </label>
-                              <p className="text-gray-500">Monthly reminders of when your payments are due.</p>
-                           </div>
-                        </div>
-                     </div>
-                  </fieldset>
-                  <fieldset>
-                     <legend className="text-sm font-semibold leading-6 text-gray-900">Push Notifications</legend>
-                     <p className="mt-1 text-sm leading-6 text-gray-600">
-                        These are delivered via SMS to your mobile phone.
-                     </p>
-                     <div className="mt-6 space-y-6">
-                        <div className="flex items-center gap-x-3">
-                           <input
-                              id="push-everything"
-                              name="push-notifications"
-                              type="radio"
-                              className="h-4 w-4 border-gray-300"
-                           />
-                           <label htmlFor="push-everything" className="block text-sm font-medium leading-6 text-gray-900">
-                              Everything
-                           </label>
-                        </div>
-                        <div className="flex items-center gap-x-3">
-                           <input
-                              id="push-email"
-                              name="push-notifications"
-                              type="radio"
-                              className="h-4 w-4 border-gray-300"
-                           />
-                           <label htmlFor="push-email" className="block text-sm font-medium leading-6 text-gray-900">
-                              Same as email
-                           </label>
-                        </div>
-                        <div className="flex items-center gap-x-3">
-                           <input
-                              id="push-nothing"
-                              name="push-notifications"
-                              type="radio"
-                              className="h-4 w-4 border-gray-300"
-                           />
-                           <label htmlFor="push-nothing" className="block text-sm font-medium leading-6 text-gray-900">
-                              No push notifications
-                           </label>
-                        </div>
-                     </div>
-                  </fieldset>
-               </div>
-            </div>
-
             {/* Error message for incomplete form */}
             {formError && <p className="text-red-500 text-sm mt-2">Please fill out all fields of information.</p>}
 
